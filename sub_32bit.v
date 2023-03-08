e56dfa239d1a34107b659a087abb73399201c061
DavidHoernke <david.hoernke23@gmail.com>
2023-03-07 19:49:31 -0500

    fixed some subtract stuff

--- sub_32bit.v
+++ sub_32bit.v
@@ -1,4 +1,4 @@
-module sub_32bit(input wire [31:0] Ra, input wire [31:0] Rb, input wire cin, output wire [31:0] sum, output wire cout);
+module sub_32bit(input  [31:0] Ra, input  [31:0] Rb, input wire cin, output wire [31:0] sum, output wire cout);
 	wire [31:0]negative_Rb;
 	negate negative(.Ra(Rb),.Rz(negative_Rb));
 	add_32bit negative_sum(.Ra(Ra), .Rb(negative_Rb), .cin(cin),.cout(cout));
