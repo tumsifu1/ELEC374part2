3880ed07edaf87774632ef4cae7c66594115293c
aaron moise <19ajgm@queensu.ca>
2023-03-08 13:37:24 -0500

    Fixed all errors

--- MDR.v
+++ MDR.v
@@ -1,15 +1,15 @@
 module MDR(input clear,
     input clock, 
     input read,
-    input MDRin, 
-    wire [31:0] BusMuxOut, 
+    input MDRin,  
     input [31:0] Mdatain, 
     output [31:0] memOut);
-
-    wire [31:0] temp;
+	
+	 wire [31:0] BusMuxOut;
+    wire [31:0] regOut;
     mux2_1 MDmux(BusMuxOut, BusMuxIn, Mdatain, read);
-    register MDRegister(clr, clock, BusMuxOut, MDRin, temp);
+    register MDRegister(clr, clock, BusMuxOut, MDRin, regOut);
 
-    assign memOut = temp;
+    assign memOut = regOut;
     
 endmodule 
