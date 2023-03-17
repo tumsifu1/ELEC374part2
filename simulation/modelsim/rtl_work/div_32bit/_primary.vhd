2e20226607eb67143b88b534706b8f86fa81204a
aaron moise <19ajgm@queensu.ca>
2023-03-17 14:40:27 -0400

    rtl work edit

--- simulation/modelsim/rtl_work/div_32bit/_primary.vhd
+++ simulation/modelsim/rtl_work/div_32bit/_primary.vhd
@@ -6,8 +6,12 @@ entity div_32bit is
         divisor         : in     vl_logic_vector(31 downto 0);
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
         quotient        : out    vl_logic_vector(31 downto 0);
         remainder       : out    vl_logic_vector(31 downto 0)
+=======
+        quotient        : out    vl_logic_vector(31 downto 0)
+>>>>>>> 6889b348620290086cf5e8c8e14ea4fa5f240fa6
 =======
         quotient        : out    vl_logic_vector(31 downto 0)
 >>>>>>> 6889b348620290086cf5e8c8e14ea4fa5f240fa6
